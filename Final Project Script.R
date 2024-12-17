d0255c77701605acc28d5c87b0c6858bf35b9e36
LeslyRamos <r12627041@ntu.edu.tw>
2024-12-17 23:24:46 +0800

    Final

--- Final Project Script.R
+++ Final Project Script.R
@@ -97,7 +97,10 @@ ggplot(fish_data, aes(x = Fish_Response)) +
     x = "Fish Response Category",
     y = "Count"
   ) +
-  theme_minimal()
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
 
 
 
